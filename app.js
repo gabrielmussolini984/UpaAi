19a8c873b587a7fbafcb2835a6a470731db42f9c
gabrielmussolini984 <gabrielmussolini@hotmail.com>
2020-01-21 12:00:25 -0300

    Alterando Limite de Upload

--- app.js
+++ app.js
@@ -5,77 +5,61 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 const formidableMiddleware = require('express-formidable');
 const fs = require('fs').promises;
-const flash = require('connect-flash');
+const flash = require('express-flash');
 const session = require('express-session');
-
-
+const { middlewareGlobal} = require('./middlewares/middleware');
 
 // Init App
 const app = express();
-
-app.use(formidableMiddleware({
-  encoding: 'utf-8',
-  uploadDir: './'
-}));
-
-app.use(session({
-  secret: "cursodenode",
-  resave: true,
-  saveUninitialized: true
-}));
-// Midleware
-app.use(flash());
-app.use((req,res,next)=>{
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next(); 
-})
 
 // HandleBars Config
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
+
 // BodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Configure Formidable
+app.use(formidableMiddleware({
+  encoding: 'utf-8',
+  uploadDir: path.resolve(__dirname, 'arquivos'),
+  maxFileSize: 1 * 1024 * 1024
+}));
+
 // Public
-//app.use(express.static('public'))
 app.use(express.static(path.join(__dirname,'assets')));
+
+// Session
+app.use(session({
+  secret: 'kasdasdasdasdcat',
+  resave: false,
+  saveUninitialized: true,
+}))
+// Flash Messages
+app.use(flash());
+// MiddleWare Global
+app.use(middlewareGlobal);
+
 
 // Rota Default
 app.get('/', (req, res)=>{
-  req.flash('success_msg', 'Arquivo Copiado com Sucesso')
-  res.render('home');
+  res.render('home')
 });
-
-app.post('/upaai', (req,res)=>{
+app.post('/upaai', async (req,res)=>{
   let nomeTemporario = req.files.arquivo.path
   let nomeCorreto = req.files.arquivo.name 
-  const renomeando = fs.rename('./'+nomeTemporario,'./'+nomeCorreto)
-  renomeando
-  .then(()=>{
-    console.log('Arquivo Salvo com sucesso')
-    req.flash('success_msg', 'Arquivo Copiado com Sucesso')
-    //res.render('home')
-    res.redirect('/#work')
-  })
-  .catch(err => {
-    console.log('Erro ao salvar: '+err)
-    let status = 'Erro ao Enviar Arquivo'
-    res.render('/home', {status})
-    
-  })
-  //console.log(renomeando)
-  
-  
+  try {
+    await fs.rename(path.resolve(__dirname, 'arquivos', nomeTemporario),path.resolve(__dirname, 'arquivos', nomeCorreto), );
+    //await fs.rename(nomeTemporario,nomeCorreto);
+    req.flash('success', 'Arquivo Enviado com Sucesso');  
+    return res.redirect('/#work');
+  } catch (error) {
+    console.log(error);
+    req.flash('error', 'Erro ao Enviar o Arquivo');
+  }
 })
-app.get('#folder', (req, res)=>{
-  console.log('teste')
-  res.send('sda')
-});
-
 
 // Server Running
 const port = process.env.PORT || 3000;
